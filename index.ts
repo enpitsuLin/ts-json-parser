0cd619779503a15725959981786ac6dc0de1788f
enpitsulin <enpitsulin@gmail.com>
2022-05-19 15:59:56 +0800

    refactor

--- index.ts
+++ index.ts
@@ -1,31 +1,4 @@
-import type { Equal, Expect } from '@type-challenges/utils'
 import type { Parser } from './parser'
 import type { Tokenize } from './tokenizer'
 
-type TokenType =
-  | 'BEGIN_OBJECT'
-  | 'END_OBJECT'
-  | 'BEGIN_ARRAY'
-  | 'END_ARRAY'
-  | 'NULL'
-  | 'NUMBER'
-  | 'STRING'
-  | 'BOOLEAN'
-  | 'SEP_COLON'
-  | 'SEP_COMMA'
-
-type _Test_Array = Parser<['[', 'null', ',', '{', 'foo', ':', 'bar', '}', ']']>
-type _Test_Object = Parser<['{', 'foo', ':', 'null', ',', 'bar', ':', '[', 'true', ',', 'false', ',', '2', ']', '}']>
-
 type ParserTest = Parser<Tokenize<'{"foo":"null","bar":["true","false","2"]}'>>
-
-type cases = [
-  Expect<
-    Equal<
-      Tokenize<'{"foo":"null","bar":["true","false","2"]}'>,
-      ['{', 'foo', ':', 'null', ',', 'bar', ':', '[', 'true', ',', 'false', ',', '2', ']', '}']
-    >
-  >
-]
-
-export {}
--- tokenizer.ts
+++ tokenizer.ts
@@ -1,31 +1,3 @@
 import type { TokenTypes, Numeric } from './basic'
 
-/**
- * Tokenize
- * - When char is number will be **never**, a number.
- * - Parsing the numbers is technically possible, but compiler will throw Type instantiation is excessively deep.
- */
-export type Tokenize<Input extends string> = Input extends `${infer F}${infer U}`
-  ? F extends TokenTypes['BEGIN_OBJECT']
-    ? [TokenTypes['BEGIN_OBJECT'], ...Tokenize<U>]
-    : F extends TokenTypes['END_OBJECT']
-    ? [TokenTypes['END_OBJECT'], ...Tokenize<U>]
-    : F extends TokenTypes['BEGIN_ARRAY']
-    ? [TokenTypes['BEGIN_ARRAY'], ...Tokenize<U>]
-    : F extends TokenTypes['END_ARRAY']
-    ? [TokenTypes['END_ARRAY'], ...Tokenize<U>]
-    : F extends TokenTypes['SEP_COLON']
-    ? [TokenTypes['SEP_COLON'], ...Tokenize<U>]
-    : F extends TokenTypes['SEP_COMMA']
-    ? [TokenTypes['SEP_COMMA'], ...Tokenize<U>]
-    : F extends Numeric
-    ? never
-    : F extends ' '
-    ? Tokenize<U>
-    : ReadingString<`${F}${U}`>
-  : []
-
-/**
- * Get a first parsed string from S
- */
-type ReadingString<String extends string> = String extends `"${infer U}"${infer Rest}` ? [U, ...Tokenize<Rest>] : never
+export type Tokenize<Input extends string> = Input
